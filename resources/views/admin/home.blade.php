9cc45f6451e6161baf073dbc42d0d5219c584d8a
KobayashiTomoya <kobayashi.tomoya@moneyforward.co.jp>
2020-10-05 10:13:13 +0900

    日勤・公休・有給のセルの追加

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -21,6 +21,9 @@
                     <tr>
                         <td style="border:1px solid black; padding:1em;"></td>
                         @include('admin.daytd')
+                        <td style="border:1px solid black; padding:1em;" rowspan="2">日勤</td>
+                        <td style="border:1px solid black; padding:1em;" rowspan="2">公休</td>
+                        <td style="border:1px solid black; padding:1em;" rowspan="2">有給</td>
                     </tr>
                     <tr>
                         <td style="border:1px solid black; padding:1em;"></td>
