9ec1942bcefc012ce3bd3b83b94a8bf6840109d2
KobayashiTomoya <kobayashi.tomoya@moneyforward.co.jp>
2020-10-05 10:57:52 +0900

    リファクタリングしユーザーに基づく有給が表示できた

--- resources/views/admin/userdescriptiontd.blade.php
+++ resources/views/admin/userdescriptiontd.blade.php
@@ -2,8 +2,9 @@
   <td
   class="<?php echo ($dayobj->is_sunday) ? 'is_sunday' : ''; ?>  <?php echo ($dayobj->is_public_holiday) ? 'is_public_holiday' : ''; ?>" 
   style="border:1px solid black; padding:1em;">
-  @foreach($holidays->getSundayHoliday() as $holiday)
-    @if($dayobj->day->format('Y-m-d') == $holiday->day)
+  
+  @foreach($user->holidays as $holiday)
+    @if($holiday->day == $dayobj->day->format('Y-m-d'))
       {{ $holiday->description }}
     @endif
   @endforeach
